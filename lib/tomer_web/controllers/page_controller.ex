3805b92cbb4508709005bce9217e504457f0232d
boomchanotai <boom9387@hotmail.com>
2024-07-19 21:06:23 +0700

    feat: add live

--- lib/tomer_web/controllers/page_controller.ex
+++ lib/tomer_web/controllers/page_controller.ex
@@ -1,14 +1,15 @@
 defmodule TomerWeb.PageController do
   use TomerWeb, :controller
 
-  def home(conn, _params) do
+  def home(conn, params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false, isAdmin: false)
+    isLive = Map.get(params, "isLive", "false")
+    render(conn, :home, layout: false, isAdmin: false, isLive: isLive == "true")
   end
 
   def admin(conn, _params) do
     state = Tomer.Room.get()
-    render(conn, :home, layout: false, isAdmin: true, state: state)
+    render(conn, :home, layout: false, isAdmin: true, state: state, isLive: false)
   end
 end
